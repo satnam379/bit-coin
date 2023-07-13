5ea78968e6a5a5325713846f704ba7a90889b1e9
satnam <satnam@gmail.com>
2023-07-13 15:19:17 +0530

    changes added

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ const Stack = createNativeStackNavigator();
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Screen1 from "./screens/Screen1";
 import Screen2 from "./screens/Screen2";
+import Screen3 from "./screens/Screen3";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View, Text, Pressable, TouchableOpacity } from "react-native";
@@ -16,8 +18,18 @@ const App = () => {
         {hideSplashScreen ? (
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen
+              name="Screen1"
+              component={Screen1}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
               name="Screen2"
               component={Screen2}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Screen3"
+              component={Screen3}
               options={{ headerShown: false }}
             />
           </Stack.Navigator>
